9e6ad0829d3c645a1e66bc1fa0b73b63208be4dd
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-11 17:10:02 -0600

    56. Step 6: Show off your work (#2)

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+	@IBOutlet weak var imageView: UIImageView!
+		
+	@IBAction func askButtonPressed(_ sender: Any) {
+		imageView.image = ballArray[Int.random(in: 0 ... 4)]
+		showImage()
+	}
+	
+    private let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
+	@objc func showImage() {
+			// Fade the image in (make it visible)
+			UIView.animate(withDuration: 1.0, animations: {
+				self.imageView.alpha = 1.0
+			}) { _ in
+				// After 3 seconds, fade the image out
+				DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+					UIView.animate(withDuration: 1.0, animations: {
+						self.imageView.alpha = 0.0
+					})
+				}
+			}
+		}
 
 }
 
